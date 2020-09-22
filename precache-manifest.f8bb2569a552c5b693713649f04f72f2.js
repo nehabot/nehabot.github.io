aff16d8958269cc8668e504d2b5a48adc841400d
RoBlockHead <student5@daniellpedia.com>
2020-09-21 21:46:44 -0400

    Updates

---
+++ precache-manifest.f8bb2569a552c5b693713649f04f72f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366ed274227a37c95bcd697a6432ca5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d13c029a7955331a4abf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8217dc888ff2dacef58b",
+    "url": "/static/js/2.eb72d9f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb72d9f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13c029a7955331a4abf",
+    "url": "/static/js/main.a319f217.chunk.js"
+  },
+  {
+    "revision": "64196f790e8b18312d93",
+    "url": "/static/js/runtime-main.ba563fed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
